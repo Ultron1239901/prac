8725f37174e5dbb8ac1cdfbdb463675669d29609
Ultron1239901 <vaibhavdhrudev@gmail.com>
2026-02-15 00:13:25 +0530

    Added multiply feature

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-def add(a, b):
-    return a + b
+def multiply(a, b):
+    return a * b
 
-def subtract(a, b):
-    return a - b
-
-print("Add:", add(5, 3))
-print("Subtract:", subtract(5, 3))
+print("Multiply:", multiply(5, 3))
